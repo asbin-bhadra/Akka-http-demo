792801401ae1ba14de33c62c8364d12bf2be426a
knoldus <asbin.bhadra@knoldus.com>
2021-05-26 15:19:10 +0530

    Akka HTTP demo

---
+++ build.sbt
@@ -0,0 +1,13 @@
+name := "Akka-http_demo"
+
+version := "0.1"
+
+scalaVersion := "2.12.12"
+
+val AkkaVersion = "2.6.8"
+val AkkaHttpVersion = "10.2.4"
+
+libraryDependencies ++= Seq(
+  "com.typesafe.akka" %% "akka-stream" % AkkaVersion,
+  "com.typesafe.akka" %% "akka-http" % AkkaHttpVersion
+)
---
+++ src/main/scala/org/knoldus/HttpServer.scala
@@ -0,0 +1,42 @@
+package org.knoldus
+
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.StatusCodes
+import akka.stream.ActorMaterializer
+import akka.http.scaladsl.server.Directives._
+
+import scala.util.{Failure, Success}
+
+
+object HttpServer extends App {
+
+  val host = "localhost"
+  val port = 7000
+
+  implicit val system = ActorSystem("HTTP_SERVER")
+  implicit val materializer : ActorMaterializer.type = ActorMaterializer
+  import system.dispatcher
+
+  val route =
+    pathPrefix("api"){
+      get{
+        path("hello"){
+          complete(StatusCodes.OK, "Hello from Server")
+        }
+      }
+    }
+
+  val binding = Http().newServerAt(host,port).bindFlow(route)
+
+  binding.onComplete{
+      case Success(value) =>
+        println(s"Server is listening on http://$host:${port}")
+      case Failure(exception) =>
+        println(s"Failure : $exception")
+        system.terminate()
+    }
+
+
+
+}
